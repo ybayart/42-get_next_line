0db1e986adc60d7a0eb8842bb3b5679b74462fef
Yann BAYART <ybayart@e1r1p1.42.fr>
2019-11-13 20:20:47 +0100

    modif

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ybayart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 19:21:46 by ybayart           #+#    #+#             */
-/*   Updated: 2019/11/10 20:37:04 by ybayart          ###   ########.fr       */
+/*   Updated: 2019/11/13 20:12:42 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int		main(void)
+int		main(int argc, char **argv)
 {
 	char	*line;
 	int		file;
-	int		i;
-	int		state;
 
-//	file = open("get_next_line.c", O_RDWR);
-	file = 0;
-	i = -1;
-	while ((state = get_next_line(file, &line)) == 1)
+	file = open(argv[1], O_RDWR);
+	while (get_next_line(file, &line))
 	{
-		printf("%d: %s\n", state, line);
+		printf("%s\n",line);
 		free(line);
-//		printf("-> line\n");
 	}
-//	close(file);
-//	free(line);
 	while(1);
 }
